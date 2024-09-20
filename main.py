ede0d912a1703ee2ba47f905e07e8562ea3324f3
Erik Bayerlein <erik.bayer@icloud.com>
2024-09-20 17:41:42 -0300

    adding input and prints

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from protocol.inputReader.input_reader import InputReader
 from protocol.scheduler.scheduler import Scheduler
 
 def main():
-    entrada_ai = "r4(v)r3(y)r1(y)r1(x)w2(u)r2(x)w1(y)r2(y)c2c1w4(u)r3(x)c4w3(u)w3(z)c3"
-    # entrada_ai = "w2(u)w3(u)w4(u)w4(x)w2(x)"
+    entrada_ai = "r2(v)r1(x)w2(x)r3(v)r1(y)w3(y)r2(z)w3(z)c3c1c2"
 
     operations = InputReader.read_input(entrada_ai)
     for op in operations:
-        print(f"{op.get_id()}, {op.get_type()}, {op.object}")
+        print(f"{op.get_id()}, {op.get_transaction_id()}, {op.get_type()}, {op.object}")
     scheduler = Scheduler()
-    print(scheduler.schedule(operations))
+    op = scheduler.schedule(operations)
+    for s in op:
+        print(s)
 
 
 if __name__ == "__main__":
